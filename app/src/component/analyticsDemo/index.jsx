aadbf426060116e7b60b8e70d2d81f42e7d0e819
Igor Bruno Pereira Soares <ibp.srs@gmail.com>
2018-02-12 11:50:41 -0200

    Terminal styling the analytics demo output div

--- app/src/component/analyticsDemo/index.jsx
+++ app/src/component/analyticsDemo/index.jsx
@@ -21,6 +21,8 @@ const mapDispatchToProps = dispatch => { return {} }
 @connect(mapStateToProps, mapDispatchToProps)
 class analyticsDemo extends React.PureComponent {
 
+  // The innerScrollDiv must be rendered first, and only then, its inner elements are added
+  // The reference container must exist int the page DOM before the the visibility-detectable items are rendered
   componentDidMount() {
     var elements = [
       <VisibilityDetected name='upper' visibilityContainer={this.innerScrollDiv}/>,
@@ -35,26 +37,47 @@ class analyticsDemo extends React.PureComponent {
   }
 
   render () {
-    const divStyle = { height: '200px', width: '100%', overflowY: 'auto', wordBreak: 'break-all'}
+    const terminalStyle = {
+      height: '200px',
+      width: '100%',
+      overflowY: 'auto',
+      wordBreak: 'break-all',
+      margin: 0,
+      padding: '5px',
+      listStyle: 'none',
+      background: '#141414',
+      color: 'white',
+      font: '0.8em',
+      lineHeight: '1.6em',
+      borderBottomRightRadius: '3px',
+      borderBottomLeftRadius: '3px'
+    }
+
+    const actionNameStyle = {
+      color: '#45D40C'
+    }
 
     const logDiv =
-      <div style={divStyle} ref={(el) => { this.messages = el; }}>
-        <ul>
-          {this.props.log.map(i => <li>{i}</li>)}
+      <div style={terminalStyle} ref={(el) => { this.messages = el; }}>
+        <ul style={{listStyleType: 'none'}}>
+          {this.props.log.map(i => (
+            <li>
+              <span style={actionNameStyle}>{i.type} :: </span>
+              <span>{JSON.stringify(i.data)}</span>
+            </li>
+          ))}
         </ul>
         <div style={{ float:'left', clear: 'both' }}
              ref={(el) => { this.messagesEnd = el; }}>
         </div>
       </div>
 
-    // The innerScrollDiv must be rendered first, and only then, its inner elements are added
-    // The reference container must exist before the the visibility-detectable items are rendered
     return (
       <components.Box>
         <h2>
           Feature: <i>Analytics demo</i>
         </h2>
-        <div ref={el => this.innerScrollDiv = el} style={{...divStyle, height: '100px'}}>
+        <div ref={el => this.innerScrollDiv = el} style={{height: '100px', overflowY: 'auto'}}>
           {this.state && this.state.visibleChildren}
         </div>
         {logDiv}
--- app/src/service/analyticsDemo/reducer.js
+++ app/src/service/analyticsDemo/reducer.js
@@ -2,7 +2,7 @@ const reducer = (state = {log: []}, action) => {
   const { type, data } = action
   if (!type.endsWith('_RESPONSE')) {
     var log = state.log.slice(0)
-    log.push(JSON.stringify({type, data}))
+    log.push({type, data})
     if (log.length > 40) {
       log.shift()
     }
