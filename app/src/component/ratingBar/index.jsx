2df15642ee9dfd0a326f5d39d4a9b07a11a2abbe
DaveT <dthorneycroft@goodpractice.com>
2018-03-06 10:54:05 +0000

    Refactored the index.js file to enable standalone feature for rating bar

--- app/src/component/ratingBar/index.jsx
+++ app/src/component/ratingBar/index.jsx
@@ -1,158 +1,77 @@
 import React, {Component} from 'react'
-import {findDOMNode} from 'react-dom'
-import propTypes from 'prop-types'
-import RatingSymbol from './RatingSymbol'
-import FlatButton from 'material-ui/FlatButton';
+import { connect } from 'react-redux'
+import { Card, CardTitle, CardText, CardMedia, CardActions } from 'material-ui/Card'
+import { services, components, actionHub } from '../../loader'
 
-const {func, number, object, oneOf, oneOfType, string} = propTypes
-const defaultDirection = 'left'
-const ratedStyle = {
-  fontSize: '0.65em',
-  fontStyle: 'italic'
+const styles = {
+  ratingWrapper: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  ratingCard: {
+    marginLeft: 20,
+    marginTop: 20,
+    maxWidth: '30%'
+  }
 }
+
+const product  =  {
+  id: 1,
+     imageURL:'https://i.ebayimg.com/images/g/pJoAAOSwx~JWE2t0/s-l300.jpg',
+     name: 'BB8 Star Wars Ep VII',
+     price: 10
+   }
 
 class component extends Component {
 
-  constructor(props){
-    super(props)
-    this.state = { rating: 0}
+  submitRating = (rating) => {
+    this.props.rateProduct(rating)
   }
-
-  componentWillMount(){
-    this.thresholdSymbol = null
-  }
-
-  componentDidMount(){
-    let symbol = findDOMNode(this.thresholdSymbol)
-    this.symbolBoundingClientRec = symbol.getBoundingClientRect()
-    this.mousePosition = this.symbolBoundingClientRec
-  }
-
-  _onContainerMouseLeave = (e) => {
-      this.mousePosition = e.target.getBoundingClientRect()
-      this._setRating(0)
-  }
-  _onContainerMouseMove = (e) => {
-      this.mousePosition = e.target.getBoundingClientRect()
-  }
-
-  _handleMouseMove = (value) => {
-    let {rating} = this.state
-
-    if(value !== rating ){
-
-      this._setRating(value)
-    }
-  }
-
-   _handleOnClick = () => {
-
-     let {rating} = this.state
-     let {description, disabled, onClick} = this.props
-
-     if (disabled) {
-       e.stopPropagation()
-       e.preventDefault()
-       return false
-
-     } else {
-         if(onClick){
-           onClick({rating, description})
-         }
-     }
-   }
-
-   _setRating = (value) => {
-      this._isValid() ? this.setState({rating: value}):this.setState({rating:0})
-   }
-
-   _isValid = () => {
-     let {direction} = this.props
-     let {mousePosition, symbolBoundingClientRec} = this
-     return direction === defaultDirection ? (mousePosition.left) > symbolBoundingClientRec.left  : (mousePosition.right) <  symbolBoundingClientRec.right
-   }
-
-   _generateIcons = (number) => {
-
-       let {description, direction, iconNumber, symbolContainerStyle, symbolStyle} = this.props
-       let {rating} = this.state
-       let refIndex = direction === defaultDirection ? 0 : iconNumber-1
-       return Array(number).fill(null).map((n, index) => {
-        let value = index + 1
-        let active = rating === 0 ? false  : value <= rating
-
-        const defaultProps = {
-           key: index,
-           active,
-           description,
-           value,
-           symbolContainerStyle,
-           symbolStyle,
-           onClick: this._handleOnClick,
-           onMouseMove: this._handleMouseMove
-         }
-
-         return (index === refIndex ?
-           <RatingSymbol
-             ref={(symbol) => {this.thresholdSymbol = symbol}}
-             {...defaultProps}
-             />
-             :
-             <RatingSymbol
-               {...defaultProps}
-               />
-             )
-           }
-         )
-       }
 
    render() {
 
-     let {disabled, iconNumber, btnLabel, btnLabelStyle} = this.props
-     let {rating} = this.state
+     const {ratingWrapper, ratingCard} = styles
+     let{ ratedProducts, numberOfReviews} = this.props
 
      return (
-      <div ref={(container) => {this.symbolContainer = container}} style={{marginLeft: 5 , marginRight: 5}} onMouseLeave={this._onContainerMouseLeave}>
-          <div onMouseMove={this._onContainerMouseMove} >
-          {
-            btnLabel &&
-            <FlatButton label={btnLabel} disabled={disabled} onClick={this._handleOnClick} labelStyle={btnLabelStyle}/>
-          }
-          <div style={{display: 'flex', 'flexDirection': 'row', marginLeft: 5 , marginRight: 5}}>
-            { iconNumber > 0 &&
-              this._generateIcons(iconNumber)
-            }
-          </div>
-          {
-            rating > 0 ?
-            <span style={ratedStyle}>{`${rating} people have rated this item`}</span>
-            :
-            <span style={ratedStyle}>{'Be the first to rate this item'}</span>
-          }
-        </div>
-      </div>
+       <div style={ratingWrapper}>
+         <Card style={ratingCard}>
+           <CardTitle title={product.name} />
+           <CardMedia>
+             <img src={product.imageURL} style={{display: 'block', margin: 'auto'}} />
+            </CardMedia>
+            <CardTitle title={`$ ${product.price}`} />
+            <CardText>
+              <components.rating
+                iconNumber={5}
+                btnLabel='Rate this item'
+                description={product}
+                ratedProducts={ratedProducts}
+                numberOfReviews={numberOfReviews}
+                onClick={this.submitRating}
+                symbolContainerStyle={{width:30}} />
+            </CardText>
+       </Card>
+     </div>
      );
    }
  }
 
-component.propTypes = {
-  btnLabel: string,
-  btnLabelStyle: object,
-  description: oneOfType([object, string]),
-  direction: oneOf(['right', 'left']),
-  onClick: func,
-  onMouseMove: func,
-  symbolContainerStyle: object,
-  symbolStyle: object
-}
+ const mapStateToProps = (state) => {
+   let {shopping:{products=[]}} = state
+   const {ratingBar:{selector:{getProductRatingScores, getNumberOfProductReviewers}}} = services
+   return{
+     ratedProducts: getProductRatingScores(state, products),
+     numberOfReviews: getNumberOfProductReviewers(state, products),
+   }
+ }
 
-component.defaultProps = {
-  btnLabel: string.isRequired,
-  onClick: func.isRequired,
-  btnLabelStyle: {fontSize: 10},
-  direction: 'left',
-  symbolContainerStyle: {width: 20},
-  symbolStyle: {}
-}
+ const mapDispatchToProps = (dispatch) => {
+   const {RATING_BAR_ADD_RATING} = actionHub
+   console.log(actionHub)
+   return{
+     rateProduct: RATING_BAR_ADD_RATING
+   }
+ }
 
-export default component
+export default connect(mapStateToProps, mapDispatchToProps)(component)
