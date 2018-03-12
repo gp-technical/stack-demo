4d2a93796c2b7c98ddc48e31c15e185b4a564671
biofractal <biofractal@gmail.com>
2018-03-12 15:59:30 +0000

    test

--- api/src/service/errors/processor.js
+++ api/src/service/errors/processor.js
@@ -1,10 +1,10 @@
-import {makeProcessor} from '@gp-technical/stack-pack-api'
+import { makeProcessor } from '@gp-technical/stack-pack-api'
 
-const processor = async (action) => {
-  var {types, type, data} = action
+const processor = async action => {
+  var { types, type, data } = action
   switch (type) {
     case types.errorsThrowFromApi:
-      throw(new Error('This is a test error that was thrown by the stack-demo API'))
+      throw new Error('This is a test error that was thrown by the stack-demo API')
   }
 }
 
