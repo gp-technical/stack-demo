820d3888dd9c0ea9eb1bc289c9c4188bdc83b153
biofractal <biofractal@gmail.com>
2018-05-13 13:07:22 +0100

    package swap local

--- swap.js
+++ swap.js
@@ -8,8 +8,6 @@ swap('api')
 function swap(dir) {
   const path = `${__dirname}/${dir}`
   const target = process.argv[2]
-  console.info('path', path)
-  console.info('target', target)
   exec(`sp-swap ${path} ${target}`, (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`)
