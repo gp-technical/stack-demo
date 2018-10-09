a739cfdebf8779123c85f711cba7213cc1785a09
Arthur Seaton <aseaton@goodpractice.com>
2018-10-09 14:32:41 +0100

    Starting removing boilerplate

--- api/src/index.js
+++ api/src/index.js
@@ -1,16 +1,22 @@
 import 'babel-polyfill'
 import { stack } from '@gp-technical/stack-pack-api'
-import services from './service'
+import { featureServices } from '@gp-technical/stack-feature-api'
+import localServices from './service'
+import { saml } from '@gp-technical/stack-auth-api'
 // import winston from 'winston'
 // import util from 'util'
+
+const { branding } = featureServices
+const services = { branding, ...localServices }
 
 // winston.add(winston.transports.Logentries, { token: process.env.API_LOGENTRIES_TOKEN })
 ;(async () => {
   try {
-    debugger
     await stack.connect({
       dir: __dirname, // @@@ make this less crap
-      services
+      services,
+      gpapi: true,
+      authenticator: saml
     })
   } catch (inner) {
     const err = new Error(`An error occurred whilst starting the ${process.env.API_NAME} API`)
