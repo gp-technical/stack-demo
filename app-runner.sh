2f58b96aaa320892834d6f7f070283b5243a3445
Arthur Seaton <aseaton@goodpractice.com>
2018-11-13 13:05:49 +0000

    Update app_runner

--- app-runner.sh
+++ app-runner.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-sed -i "s#wss://stack-demo.local.goodpractice.net:3001#$SOCKET_SERVER#g" bundle.js
+if [ -f index.html ]; then
+  echo 'moving files to app folder'
+  mkdir -p app
+  mv bundle.js app
+  mv index.html app
+  mv style.css app
+
+  sed -i "s#wss://stack-demo.local.goodpractice.net:3001#$SOCKET_SERVER#g" app/bundle.js
+fi
 
 pm2 start process.json --no-daemon
